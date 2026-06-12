10cc2f8f552a6228ef279267125aa4069030b0a3
dommarajuJeevana <dommaraju.jeevana@gmail.com>
2026-06-12 11:52:24 +0530

    modified

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecommerce-app-otze.onrender.com";
+const BASE_URL = "https://ecommerce-app-otze.onrender.com/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
